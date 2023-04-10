3a9144ef7e31d43a5e5a622c82bed64a42fa4b2c
MichMendez1 <101906343+MichMendez1@users.noreply.github.com>
2023-04-10 02:37:42 -0400

    Primer commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {createBrowserRouter,RouterProvider} from 'react-router-dom'
+import Login from './views/login';
+
+const router = createBrowserRouter([
+  {
+    path:'/',
+    Component:App
+  },
+  {
+    path:'/login',
+    Component:Login
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
