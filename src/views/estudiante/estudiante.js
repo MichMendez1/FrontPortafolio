dfa1841aa3b4f501e1fa40e27482396a2aabbe30
ScarlethM <sc.martinezg@duocuc.cl>
2023-05-20 20:56:37 -0400

    holi

---
+++ src/views/estudiante/estudiante.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+const estudiante = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default estudianteS
